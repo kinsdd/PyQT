9bb343ca8299d330ddb2ef4698afe6afb7973f07
= <=>
2019-03-31 13:03:26 +0700

    add label and image

--- blank form.py
+++ blank form.py
@@ -1,10 +1,16 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 
 def window():
 	app = QtWidgets.QApplication(sys.argv)
 	w = QtWidgets.QWidget()
+	l1 = QtWidgets.QLabel(w)
+	l2 = QtWidgets.QLabel(w)
+	l1.setText("Demo")
+	l2.setPixmap(QtGui.QPixmap('re.jpg'))
 	w.setWindowTitle("Demo")
+	l1.move(100, 20)
+	l2.move(120, 90)
 	w.show()
 	sys.exit(app.exec_())
 	
